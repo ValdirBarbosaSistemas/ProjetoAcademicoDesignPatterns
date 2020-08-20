69f287e9cde762b8a59e3da002b280d84197e187
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-08-19 22:05:16 -0300

    TesteNetBeansFacade

---
+++ ProjetoDesignPatterns/src/main/java/com/mycompany/projetodesignpatterns/facade/ExemploFacade.java
@@ -0,0 +1,11 @@
+package com.mycompany.projetodesignpatterns.facade;
+
+/**
+ *
+ * @author valdir-sistemas
+ */
+public class ExemploFacade {
+    public static void main(String[] args) {
+        
+    }
+}
