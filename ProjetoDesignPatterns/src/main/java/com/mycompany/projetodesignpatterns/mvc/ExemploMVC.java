f52fc772ca653a95af9c24981d650a0e80b0d7d3
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-08-19 21:54:29 -0300

    TesteNetBeans

---
+++ ProjetoDesignPatterns/src/main/java/com/mycompany/projetodesignpatterns/mvc/ExemploMVC.java
@@ -0,0 +1,11 @@
+package com.mycompany.projetodesignpatterns.mvc;
+
+/**
+ *
+ * @author valdir-sistemas
+ */
+public class ExemploMVC {
+    public static void main(String[] args) {
+        System.out.println("ExemploMVC");
+    }
+}
