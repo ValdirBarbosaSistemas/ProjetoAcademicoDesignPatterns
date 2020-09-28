14fc8cd97a708b96300e86e38730dfdfb443fe7d
ValdirBarbosaSistemas <valdir.barbosasistemas@gmail.com>
2020-09-28 15:27:02 -0300

    Mudanças na classe Main

--- ProjetoDesignPatterns/src/main/java/com/mycompany/projetodesignpatterns/ted/tedFactoryMethod/Main.java
+++ ProjetoDesignPatterns/src/main/java/com/mycompany/projetodesignpatterns/ted/tedFactoryMethod/Main.java
@@ -3,7 +3,7 @@ package com.mycompany.projetodesignpatterns.ted.tedFactoryMethod;
 public class Main {
 	public static void main(String[] args) {
 		FactoryUsuario fabrica = new FactoryUsuario();
-		Usuarios usu = fabrica.getSexo("Indeciso");
+		Usuarios usu = fabrica.getSexo("Masculino");
 		usu.setNome("Valdir");
 		System.out.println(usu.getGenero());
 	}
